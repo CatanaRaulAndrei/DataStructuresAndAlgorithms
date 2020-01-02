940b92dbe2cf407d70a3ff0955260908ec1017d3
CatanaRaulAndrei <raul19@windowslive.com>
2020-01-02 15:21:32 +0200

    Create Alocarea dinamica a memoriei pentru un vector.cpp

---
+++ Alocarea dinamica a memoriei pentru un vector.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+//Alocarea dinamica a memoriei pentru un vector
+int main()
+{int* p=nullptr,n,temporar;
+cout<<"Dati numarul de elemente ale vectorului:";cin>>n;
+p=new int[n];
+for(int i=0;i<n;i++){
+    cout<<"Dati elementul numarul "<<i+1<<":";
+    cin>>temporar;
+    p[i]=temporar;//*(p+i)=temporar
+}
+cout<<"Elementele vecorului sunt:";
+for(int i=0;i<n;i++){
+    cout<<p[i]<<" ";//*(p+i)
+}
+delete[] p;//eliberam memoria din heap
+
+  return 0;
+}
